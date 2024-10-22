61addff517c28b79b03b65c633e2d1cc2f8b6c18
Krasimir Nikolov <krasimir.nikolov@accedia.com>
2024-10-22 18:16:03 +0300

    change title

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "Students Registry",
+      msg: "Students Registry Changed",
       students: students
     };
     res.render('home', model);
